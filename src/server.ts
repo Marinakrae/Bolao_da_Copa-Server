ca7f57eb624a00f53a7068a739dfe42235a6e4de
Marina Krause Eskelsen <marina.eskelsen@acad.ufsm.br>
2022-11-02 13:15:49 -0300

    Geração do diagrama ER

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient({
@@ -10,18 +11,16 @@ async function bootstrap() {
         logger: true,
     })
 
-    fastify.get('/pools/count', async () => {
-        const pools = await prisma.pool.findMany({
-            where: {
-                code: {
-                    startsWith: 'M'
-                }
-            }
-        })
-        return { pools }
+    await fastify.register(cors, {
+        origin: true, //colocaria aqui o dominio com permissao de acesso
     })
 
-    await fastify.listen({ port: 3333 })
+    fastify.get('/pools/count', async () => {
+        const count = await prisma.pool.count()
+        return { count }
+    })
+
+    await fastify.listen({ port: 3333, host: '0.0.0.0' })
 }
 
 bootstrap()
