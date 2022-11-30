7be1d8a8f4dc2e8de5926a82b9c7f691be7c4693
Marina Krause Eskelsen <marinakraehh@gmail.com>
2022-11-30 10:46:56 -0300

    Auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -9,5 +9,25 @@ export async function authRoutes(fastify: FastifyInstance) {
         })
 
         const { access_token } = createUserBody.parse(request.body)
-    }) 15min
+
+        const userResponse = await fetch('https://www.googleapis.com/oauth2/v2/userinfo', {
+            method: 'GET',
+            headers: {
+                Authorization: `Bearer ${access_token}`,
+            }
+        })
+
+        const userData = await userResponse.json()
+
+        const userInfoSchema = z.object({
+            id: z.string(),
+            email: z.string.email(),
+            name: z.string(),
+            picture: z.string().url(),
+        })
+
+        const userInfo = userInfoSchema.parse(userData)
+
+        return { userInfo }
+    })
 }
